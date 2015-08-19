6394e2543a7e3ad2a6c30fed29f77a407dd34e5e
pomoke <zhuodarui@gmail.com>
2015-08-19 16:39:01 +0800

    Update config.php

--- config.php
+++ config.php
@@ -13,19 +13,13 @@ $bannerAd = "";
 # List of the faucet. Change it to your own.
 $faucetList = array
   (
-  array("Halving Faucet","//halving.pw/?id=3501"),
-  array("Freebitcoin","//freebitco.in/?r=481658&tag=rotator"),
-  array("The BTC Generator","//www.thebtcgenerator.com/?id=688764"),
-  array("Moon Bitcoin","//moonbit.co.in/?ref=8b5cfd3e5715"),
-  array("Bitcoin Zebra","//faucet.bitcoinzebra.com/?ref=68508dc4a522"),
-  array("Satoshi City","//www.satoshicity.org/?r=15965"),
-  array("Faucet Cloud","//faucetcloud.com/?r=17U5uqSaceAUwwLs8fwWxRTw6U7WakZ2mw"),
-  array("InFaucet","//infaucet.com/?id=17U5uqSaceAUwwLs8fwWxRTw6U7WakZ2mw"),
-  array("Daily Bonus","//www.dailybonus.net/?r=17U5uqSaceAUwwLs8fwWxRTw6U7WakZ2mw"),
-  array("Penta Faucet","//www.pentafaucet.com/?r=17U5uqSaceAUwwLs8fwWxRTw6U7WakZ2mw"),
-  array("BitHeaven","//bitheaven.io/?r=17U5uqSaceAUwwLs8fwWxRTw6U7WakZ2mw"),
-  array("BitHell","//bithell.io/?r=17U5uqSaceAUwwLs8fwWxRTw6U7WakZ2mw"),
-  array("Free Bitcoin","//shokhibul-arifin.com/bitcoin/?r=17U5uqSaceAUwwLs8fwWxRTw6U7WakZ2mw"),
-  array("FreeBetCoin","//freebetco.in/?id=17U5uqSaceAUwwLs8fwWxRTw6U7WakZ2mw"),
-  array("ElenaBitCoin","//elenabitco.in/?id=17U5uqSaceAUwwLs8fwWxRTw6U7WakZ2mw")
+  array("Freebitcoin","//freebitco.in/?r=3676"),
+  array("The BTC Generator","//www.thebtcgenerator.com/?id="),
+  array("Moon Bitcoin","//moonbit.co.in/?ref=d2c648e77630"),
+  array("Bitcoin Zebra","//faucet.bitcoinzebra.com/?ref=56f2693fae44"),
+  array("Faucet Cloud","//faucetcloud.com/?r=1DEuQT281XNt8Y9PyJEK94cJnzJ3ZwmbSS"),
+  array("BitHeaven","//bitheaven.io/?r=1DEuQT281XNt8Y9PyJEK94cJnzJ3ZwmbSS"),
+  array("BitHell","//bithell.io/?r=1DEuQT281XNt8Y9PyJEK94cJnzJ3ZwmbSS"),
+  array("Goldspay","//goldsday.com/play?r=1DEuQT281XNt8Y9PyJEK94cJnzJ3ZwmbSS"),
+  
   );
